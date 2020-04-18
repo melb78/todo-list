b88a90784440ed6d28b60ae8d52e6137ccee9b90
David Vo <datcv@hotmail.com>
2020-04-18 19:29:23 +1000

    Add useEffect, useRef

--- src/Components/MainApp.js
+++ src/Components/MainApp.js
@@ -1,19 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { ListTodoItems } from "./ListTodoItems";
 
 export const MainApp = () => {
   // Set state
+  const [todoContent, setTodoContent] = useState("");
   const [itemList, setItemList] = useState([]);
   const [updateItemId, setUpdateItemId] = useState(-1);
+  const refContainer = useRef(null);
+
+  useEffect(() => {
+    refContainer.current.focus();
+    return () => {
+      
+    }
+  }, []);
 
   const editItem = (itemId, itemValue) => {
-    document.getElementById("textBox").value = itemValue;
+    setTodoContent(itemValue);
     setUpdateItemId(itemId);
+    refContainer.current.focus();
   };
 
   const deleteItem = (itemId) => {
     setItemList(itemList.filter((item) => item.id !== itemId));
   };
+
+  const handleChange = (event) => {
+    setTodoContent(event.target.value);
+  }
 
   const keyDownHandler = (e) => {
     if (e.key !== "Enter") {
@@ -21,30 +35,34 @@ export const MainApp = () => {
     }
 
     if (updateItemId === -1) {
-      const newItem = {
+     // Async
+      setItemList((itemList) => [...itemList, {
         id: itemList.length + 1,
-        text: document.getElementById("textBox").value,
-      };
-
-      // Async
-      setItemList((itemList) => [...itemList, newItem]);
-    } else {
+        text: todoContent,
+      }]);
+    } 
+    else 
+    {
       const foundItem = itemList.find((x) => x.id === updateItemId);
 
       if (typeof foundItem !== "undefined") {
-        foundItem.text = document.getElementById("textBox").value;
+        foundItem.text = todoContent;
       }
 
       setUpdateItemId(-1);
     }
 
-    document.getElementById("textBox").value = "";
+    setTodoContent("");
   };
 
   return (
     <div className="App">
       <header className="App-TextBox">
-        <input id="textBox" type="text" onKeyDown={keyDownHandler} />
+        <input id="textBox" type="text" 
+            ref={refContainer}
+            value={todoContent} 
+            onChange={handleChange}  
+            onKeyDown={keyDownHandler} />
       </header>
 
       <nav className="App-Label">
@@ -52,7 +70,7 @@ export const MainApp = () => {
           <span id="numTasks">
             {itemList.length}
           </span>
-          TASKS TO-DO Test CI Plus</h1>
+          TASKS TO-DO With Travis CI</h1>
       </nav>
 
       <section className="App-Collection">
