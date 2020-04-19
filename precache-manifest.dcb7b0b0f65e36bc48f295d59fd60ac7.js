8a9722fcdf2611be89be28bd501d5211becc377f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-19 04:48:41 +0000

    Deploy melb78/todo-list to github.com/melb78/todo-list.git:gh-pages

---
+++ precache-manifest.dcb7b0b0f65e36bc48f295d59fd60ac7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8f6ad673ab981eca0139e723e7641a6",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "92b5c0fd28bce4aa16b8",
+    "url": "/todo-list/static/css/main.7bfd9f52.chunk.css"
+  },
+  {
+    "revision": "b38e721c787e532dcf6a",
+    "url": "/todo-list/static/js/2.24df1694.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list/static/js/2.24df1694.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92b5c0fd28bce4aa16b8",
+    "url": "/todo-list/static/js/main.9aac4b0d.chunk.js"
+  },
+  {
+    "revision": "eba99c96cd35c495192c",
+    "url": "/todo-list/static/js/runtime-main.d593c1f4.js"
+  }
+]);
